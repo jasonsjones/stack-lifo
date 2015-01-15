d009150ebafef15330a116fbb5284dec7bba2c2e
Jason Jones <jsjones96@gmail.com>
2015-01-15 00:15:07 +0000

    implement API methods and unit tests

--- index.js
+++ index.js
@@ -8,4 +8,35 @@
 
 (function() {
     'use strict';
+
+    var LinkedList = require('dbly-linked-list');
+
+    function Stack() {
+        this._list = new LinkedList();
+    }
+
+    Stack.prototype = {
+
+        isEmpty: function () {
+            return this._list.isEmpty();
+        },
+
+        size: function () {
+            return this._list.getSize();
+        },
+
+        push: function (data) {
+            return this._list.insertFirst(data);
+        },
+
+        pop: function () {
+            return this._list.removeFirst().getData();
+        },
+
+        peek: function () {
+            return this._list.getHeadNode().getData();
+        }
+    };
+
+    module.exports = Stack;
 })();
--- test/stack-test.js
+++ test/stack-test.js
@@ -5,13 +5,71 @@ var Stack = require('../');
 
 describe('Stack Unit Tests', function() {
 
+    var stack;
+
     beforeEach(function () {
+        stack = new Stack();
     });
 
     afterEach(function () {
+        stack = null;
     });
 
     it('should have a working test environment', function() {
         true.should.equal(true);
+    });
+
+    it('should instantiate a stack instance', function () {
+        stack.should.be.ok;
+    });
+
+    it('should be empty when first instantiated', function () {
+        stack.isEmpty().should.equal(true);
+        stack.size().should.equal(0);
+    });
+
+    it('should push data onto the stack', function () {
+        stack.push('some test data');
+        stack.push('some more test data');
+        stack.push('and yet some more...');
+        stack.push({
+            "id": 1,
+            "payload": {
+                "number": 42,
+                "desc": "the answer"
+            }
+        });
+        stack.size().should.equal(4);
+    });
+
+    it('should pop data from the top of the stack', function () {
+        stack.push('some test data');
+        stack.push('some more test data');
+        stack.push('and yet some more...');
+        stack.push({
+            "id": 1,
+            "payload": {
+                "number": 42,
+                "desc": "the answer"
+            }
+        });
+        stack.size().should.equal(4);
+        var top = stack.pop();
+        JSON.stringify(top).should.equal(
+            '{"id":1,"payload":{"number":42,"desc":"the answer"}}');
+
+        stack.size().should.equal(3);
+
+        stack.pop().should.equal('and yet some more...');
+        stack.size().should.equal(2);
+    });
+
+    it('should peek at the data on top of the stack', function () {
+        stack.push('some test data');
+        stack.push('some more test data');
+        stack.push('and yet some more...');
+        stack.size().should.equal(3);
+        stack.peek().should.equal('and yet some more...');
+        stack.size().should.equal(3);
     });
 });
