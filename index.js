76519724815d9a3ff1575701885aead1313c4e44
Jason Jones <jsjones96@gmail.com>
2015-01-16 00:14:32 +0000

    add comments/documentation

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 /**
  * @fileOverview Implementation of a stack (LIFO) data structure
  * @author Jason S. Jones
@@ -9,8 +8,29 @@
 (function() {
     'use strict';
 
+    /***********************************************************
+     * Stack Data Structure
+     *
+     * This is a 'stack' data structure that implements the notion
+     * of 'Last in First Out', or LIFO.  The underlying data structure
+     * is a doubly linked list.  This linked list data structure does
+     * all the heavy lifting, enabling this implementation to be a simple
+     * wrapper around the linked list to leverage the applicable methods
+     * and properties.  This provides a very clean and simple implementation
+     * for this stack data structure.
+     *
+     ***********************************************************/
+
+    // bring in the one dependency which will be the underlying
+    // data structure for this stack implementation
     var LinkedList = require('dbly-linked-list');
 
+    /**
+     * Creates a new stack instance and initializes the underlying data
+     * structure
+     *
+     * @constructor
+     */
     function Stack() {
         this._list = new LinkedList();
     }
